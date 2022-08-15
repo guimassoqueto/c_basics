6f0ebc434aecf6b4cb36bb7421437263ee23c796
gui.massoqueto <gui.massoqueto@gmail.com>
2022-08-15 08:16:12 -0300

    update: pointers c++ & c

---
+++ c/pointer.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *p;
+    p = (int *) malloc(5 * sizeof(int));
+}
---
+++ cpp/pointer.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+    int *p;
+    p = new int[5];
+}
