4bc1b237a4285be83c01590abbea7d27dd70e546
gui.massoqueto <gui.massoqueto@gmail.com>
2022-08-15 08:16:45 -0300

    update: array c++

--- cpp/array.cpp
+++ cpp/array.cpp
@@ -7,9 +7,9 @@ int main() {
     int array[] = {10,20,30,40,50};
     int array_len = sizeof(array) / sizeof(int);
 
-    for (int i = 0;  i < array_len; i++) {
-        printf("idx: %d, val: %i\n", i, array[i]);
+    for (int i: array) {
+        printf("idx: %d, val: %i\n", i);
     }
-    
+
     return 0;
 }
