316dc925c98d205b3d8e28633aa65d000f586e6b
gui.massoqueto <gui.massoqueto@gmail.com>
2022-08-15 09:01:42 -0300

    pratice: pointer c++

---
+++ pratice/pointer1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a[5] = {10, 20, 30, 40, 50};
+    int *pointer;
+    pointer = a; // [or] pointer = &a[0];
+
+    for (int i = 0; i < 5; i++) {
+        cout << a[i] << endl;
+    }
+    return 0;
+}
