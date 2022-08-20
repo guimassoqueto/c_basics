87b7d27f5607488338b8a4ab9eab67f34a59b863
gui.massoqueto <gui.massoqueto@gmail.com>
2022-08-20 20:59:21 -0300

    feat: function call by address (c++)

---
+++ cpp/functionCallByReference.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int swap(int &num1, int &num2) {
+    int temp;
+    temp = num1;
+    num1 = num2;
+    num2 = temp;
+
+    return 0;
+}
+
+int main() {
+    int a = 1, b = 2;
+    printf("a: %d\tb: %d\n", a, b);
+
+    swap(a, b);
+    printf("a: %d\tb: %d\n", a, b);
+
+    return 0; 
+}
