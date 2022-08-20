d947f0e2c37336c4b90ca2a9a49597a8c4125718
gui.massoqueto <gui.massoqueto@gmail.com>
2022-08-20 20:58:53 -0300

    feat: Function

---
+++ cpp/function.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+int add(int number1, int number2);
+
+int main() {
+    int c = add(10, -5);
+    printf("%d", c);
+
+    return 0;
+}
+
+int add(int number1, int number2) {
+    return number1 + number2;
+}
