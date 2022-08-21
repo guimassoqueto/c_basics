d71c7742836c9662ac06ee2db752dec449e06368
gui.massoqueto <gui.massoqueto@gmail.com>
2022-08-21 17:31:16 -0300

    feat: generics scope resolution

---
+++ cpp/genericsScopeResolution.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+class Arithmetic {
+    private:
+        T n1;
+        T n2;
+    public:
+        Arithmetic(T n1, T n2);
+        T add();
+};
+
+template<class T>
+Arithmetic<T>::Arithmetic(T n1, T n2) {
+    this->n1 = n1;
+    this->n2 = n2;
+}
+
+template<class T>
+T Arithmetic<T>::add() {
+    T result = n1 + n2;
+    return result;
+}
+
+int main() {
+    Arithmetic<int> ar(5,3);
+    cout << "Sum: " << ar.add() << endl;
+}
