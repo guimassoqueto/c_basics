12578387d54d5e0ab31aa03e3c5d62800c2543d8
gui.massoqueto <gui.massoqueto@gmail.com>
2022-08-21 14:48:23 -0300

    feature: criating a new array from another

---
+++ cpp/__exercise__newArrayFromArray.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int * fun (int arraySize, int * array) {
+    int* arrayPointer;
+    arrayPointer = new int[arraySize];
+
+    for (int i = 0; i < arraySize; i++)
+    arrayPointer[i] = array[i] * 2;
+
+    return arrayPointer;
+}
+
+int main() {
+    int array[] = {5, 10, 15, 20, 25};
+    int len = sizeof(array) / sizeof(int);
+
+    int * newArr = fun(len, array);
+
+    for (int i = 0; i < len; i++) {
+        cout << newArr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
